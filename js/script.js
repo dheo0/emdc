ccb4885c78a5b584cb4babe00a5fc20caa5eb3ec
davids <dheo0@naver.com>
2021-03-20 20:21:57 +0900

    20210320

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function(){
-    
+    $(".ui-select2 > a").on("click", function(){
+        var elem = $(this).siblings("ul");
+        elem.stop().slideToggle();
+    });
+
+    $(".ui-select2 > a + ul li a").on("click", function(e){
+        e.preventDefault();
+        var selectedVal = $(this).html();
+        $(this).parent().parent().siblings("a").find("span").html(selectedVal);
+        $(this).parent().parent().slideUp();
+    })
 })
